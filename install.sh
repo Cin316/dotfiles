6f927c27cce62d22bf0845999d3ad31d318c51b1
Nicholas Reichert <nicholas@zerotosixtyfellows.com>
2018-06-19 10:10:31 -0400

    Added install.sh script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.profile ~/.profile
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
