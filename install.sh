379b74dbbd67bc17c2d34d64675c0c578e8e803e
Nicholas Reichert <joe13reich@gmail.com>
2020-09-23 19:04:15 -0400

    Changed git settings to globally ignore NicOfflineFiles.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vimrc ~/.vimrc
